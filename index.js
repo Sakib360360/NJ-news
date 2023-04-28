2ffebf075a7eabeac885b92464773569e9f65305
Sakib360360 <saakibabrar@gmail.com>
2023-04-28 17:10:43 +0600

    news updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 
 app.use(cors())
@@ -11,6 +12,27 @@ app.get('/', (req, res) => {
     res.send('hello world')
   })
 
+
+app.get('/categories/:id',(req,res)=>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const newsCategories = news.filter(n=>n.category_id === id)
+        res.send(newsCategories)
+    }
+   
+})
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id
+    console.log(id)
+    const selectedNews = news.find(n=> n._id === id)
+    res.send(selectedNews)
+})
 app.get('/categories',(req,res)=>{
     res.send(categories)
 })
